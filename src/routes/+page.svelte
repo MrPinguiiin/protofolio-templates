cfac88b8e1a083a4c308baac1a079b1950cc0f93
MrPinguiiin <alansnulien@gmail.com>
2025-11-05 16:38:11 +0700

    add: background particles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import BlurFade from '$lib/components/magic/BlurFade.svelte';
+	import Particles from '$lib/components/magic/Particles.svelte';
 	import ProjectCard from '$lib/components/portfolio/ProjectCard.svelte';
 	import ResumeCard from '$lib/components/portfolio/ResumeCard.svelte';
 	import * as Avatar from '$lib/components/ui/avatar';
@@ -34,185 +35,188 @@
 	<meta name="google-site-verification" content="your-google-verification-code" />
 	<meta name="yandex-verification" content="your-yandex-verification-code" />
 </svelte:head>
-<main class="flex min-h-[100dvh] flex-col space-y-10">
+
+<Particles className="fixed inset-0 z-0 pointer-events-none" quantity={800} staticity={10} ease={100} size={0.2} refresh={true} color="#8CCDEB" vx={0} vy={0} />
+
+<main class="relative z-10 flex min-h-[100dvh] flex-col space-y-10">
 	<section id="hero">
-		<div class="mx-auto w-full max-w-2xl space-y-8">
-			<div class="flex justify-between gap-2">
-				<div class="flex flex-1 flex-col space-y-1.5">
-					<BlurFade
-						delay={BLUR_FADE_DELAY}
-						class="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
-						yOffset={8}>Hi, I'm Muhammad Norshahlan 👋</BlurFade
-					>
-					<BlurFade class="max-w-[600px] md:text-xl" delay={BLUR_FADE_DELAY}
-						>Software Engineer turned Entrepreneur. I love building things and helping people.</BlurFade
-					>
-				</div>
-				<BlurFade delay={BLUR_FADE_DELAY}>
-					<Avatar.Root class="size-28 border">
-						<Avatar.Image alt={DATA.name} src={DATA.avatarUrl} />
-						<Avatar.Fallback>{DATA.initials}</Avatar.Fallback>
-					</Avatar.Root>
-				</BlurFade>
-			</div>
-		</div>
-	</section>
-	<section id="about">
-		<BlurFade delay={BLUR_FADE_DELAY}>
-			<h2 class="text-xl font-bold">About</h2>
-		</BlurFade>
-		<BlurFade delay={BLUR_FADE_DELAY * 1.4}>
-			<div
-				class="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert"
-			>
-				{@html marked(DATA.summary)}
-			</div>
-		</BlurFade>
-	</section>
-	<section id="work">
-		<div class="flex min-h-0 flex-col gap-y-3">
-			<BlurFade delay={BLUR_FADE_DELAY}>
-				<h2 class="text-xl font-bold">Work Experience</h2>
-			</BlurFade>
-			{#each DATA.work as work, id}
-				<BlurFade delay={BLUR_FADE_DELAY * 1.2 + id * 0.05}>
-					<ResumeCard
-						start={work.start}
-						end={work.end}
-						logoUrl={work.logoUrl}
-						company={work.company}
-						title={work.title}
-						href={work.href}
-						badges={work.badges}
-						description={work.description}
-					/>
-				</BlurFade>
-			{/each}
-		</div>
-	</section>
-	<section id="education">
-		<div class="flex min-h-0 flex-col gap-y-3">
-			<BlurFade delay={BLUR_FADE_DELAY}>
-				<h2 class="text-xl font-bold">Education</h2>
-			</BlurFade>
-			{#each DATA.education as edu, id}
-				<BlurFade delay={BLUR_FADE_DELAY * 1.2 + id * 0.05}>
-					<ResumeCard
-						start={edu.start}
-						end={edu.end}
-						href={edu.href}
-						logoUrl={edu.logoUrl}
-						company={edu.school}
-						title={edu.school}
-						subtitle={edu.degree}	
-					/>
-				</BlurFade>
-			{/each}
-		</div>
-	</section>
-	<section id="skills">
-		<div class="flex min-h-0 flex-col gap-y-3">
-			<BlurFade delay={BLUR_FADE_DELAY}>
-				<h2 class="text-xl font-bold">Skills</h2>
-			</BlurFade>
-			<div class="flex flex-wrap gap-1">
-				{#each DATA.skills as skill, id}
-					<BlurFade delay={BLUR_FADE_DELAY * id + 0.002}>
-						<Badge>{skill}</Badge>
-					</BlurFade>
-				{/each}
-			</div>
-		</div>
-	</section>
-	<section id="projects">
-		<div class="w-full space-y-12 py-12">
-			<BlurFade delay={BLUR_FADE_DELAY}>
-				<div class="flex flex-col items-center justify-center space-y-4 text-center">
-					<div class="space-y-2">
-						<div class="inline-block rounded-lg bg-foreground px-3 py-1 text-sm text-background">
-							My Projects
-						</div>
-						<h2 class="text-3xl font-bold tracking-tighter sm:text-5xl">
-							Check out my latest work
-						</h2>
-						<p
-							class="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+			<div class="mx-auto w-full max-w-2xl space-y-8">
+				<div class="flex justify-between gap-2">
+					<div class="flex flex-1 flex-col space-y-1.5">
+						<BlurFade
+							delay={BLUR_FADE_DELAY}
+							class="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
+							yOffset={8}>Hi, I'm Muhammad Norshahlan 👋</BlurFade
 						>
-							I&apos;ve worked on a variety of projects, from simple websites to complex web
-							applications. Here are a few of my favorites.
-						</p>
+						<BlurFade class="max-w-[600px] md:text-xl" delay={BLUR_FADE_DELAY}
+							>Software Engineer turned Entrepreneur. I love building things and helping people.</BlurFade
+						>
 					</div>
+					<BlurFade delay={BLUR_FADE_DELAY}>
+						<Avatar.Root class="size-28 border">
+							<Avatar.Image alt={DATA.name} src={DATA.avatarUrl} />
+							<Avatar.Fallback>{DATA.initials}</Avatar.Fallback>
+						</Avatar.Root>
+					</BlurFade>
+				</div>
+			</div>
+		</section>
+		<section id="about">
+			<BlurFade delay={BLUR_FADE_DELAY}>
+				<h2 class="text-xl font-bold">About</h2>
+			</BlurFade>
+			<BlurFade delay={BLUR_FADE_DELAY * 1.4}>
+				<div
+					class="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert"
+				>
+					{@html marked(DATA.summary)}
 				</div>
 			</BlurFade>
-			<div class="mx-auto grid max-w-[800px] grid-cols-1 gap-3 sm:grid-cols-2">
-				{#each DATA.projects as project, id}
-					<BlurFade delay={BLUR_FADE_DELAY * 1.5 + id * 0.05}>
-						<ProjectCard
-							href={project.href}
-							title={project.title}
-							description={project.description}
-							dates={project.dates}
-							tags={project.technologies}
-							image={project.image}
-							video={project.video}
-							links={project.links}
+		</section>
+		<section id="work">
+			<div class="flex min-h-0 flex-col gap-y-3">
+				<BlurFade delay={BLUR_FADE_DELAY}>
+					<h2 class="text-xl font-bold">Work Experience</h2>
+				</BlurFade>
+				{#each DATA.work as work, id}
+					<BlurFade delay={BLUR_FADE_DELAY * 1.2 + id * 0.05}>
+						<ResumeCard
+							start={work.start}
+							end={work.end}
+							logoUrl={work.logoUrl}
+							company={work.company}
+							title={work.title}
+							href={work.href}
+							badges={work.badges}
+							description={work.description}
 						/>
 					</BlurFade>
 				{/each}
 			</div>
-		</div>
-	</section>
-	<!-- <section id="hackathons">
-		<div class="w-full space-y-12 py-12">
-			<BlurFade delay={BLUR_FADE_DELAY}>
-				<div class="flex flex-col items-center justify-center space-y-4 text-center">
-					<div class="space-y-2">
-						<div class="inline-block rounded-lg bg-foreground px-3 py-1 text-sm text-background">
-							Hackathons
-						</div>
-						<h2 class="text-3xl font-bold tracking-tighter sm:text-5xl">I like building things</h2>
-						<p
-							class="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
-						>
-							During my time in university, I attended{' '}
-							{DATA.hackathons.length}+ hackathons. People from around the country would come
-							together and build incredible things in 2-3 days. It was eye-opening to see the
-							endless possibilities brought to life by a group of motivated and passionate
-							individuals.
-						</p>
-					</div>
-				</div>
-			</BlurFade>
-			<BlurFade delay={BLUR_FADE_DELAY * 2}>
-				<ul class="mb-4 ml-4 divide-y divide-dashed border-l">
-					{#each DATA.hackathons as project}
-						<BlurFade delay={BLUR_FADE_DELAY}>
-							<HackathonCard {...project} />
+		</section>
+		<section id="education">
+			<div class="flex min-h-0 flex-col gap-y-3">
+				<BlurFade delay={BLUR_FADE_DELAY}>
+					<h2 class="text-xl font-bold">Education</h2>
+				</BlurFade>
+				{#each DATA.education as edu, id}
+					<BlurFade delay={BLUR_FADE_DELAY * 1.2 + id * 0.05}>
+						<ResumeCard
+							start={edu.start}
+							end={edu.end}
+							href={edu.href}
+							logoUrl={edu.logoUrl}
+							company={edu.school}
+							title={edu.school}
+							subtitle={edu.degree}	
+						/>
+					</BlurFade>
+				{/each}
+			</div>
+		</section>
+		<section id="skills">
+			<div class="flex min-h-0 flex-col gap-y-3">
+				<BlurFade delay={BLUR_FADE_DELAY}>
+					<h2 class="text-xl font-bold">Skills</h2>
+				</BlurFade>
+				<div class="flex flex-wrap gap-1">
+					{#each DATA.skills as skill, id}
+						<BlurFade delay={BLUR_FADE_DELAY * id + 0.002}>
+							<Badge>{skill}</Badge>
 						</BlurFade>
 					{/each}
-				</ul>
-			</BlurFade>
-		</div>
-	</section> -->
-	<!-- <section id="contact">
-		<div class="grid w-full items-center justify-center gap-4 px-4 py-12 text-center md:px-6">
-			<BlurFade delay={BLUR_FADE_DELAY * 2}>
-				<div class="space-y-3">
-					<div class="inline-block rounded-lg bg-foreground px-3 py-1 text-sm text-background">
-						Contact
-					</div>
-					<h2 class="text-3xl font-bold tracking-tight sm:text-5xl">Get in Touch</h2>
-					<p
-						class="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
-					>
-						Want to chat? Just shoot me a dm
-						<a href={DATA.contact.social.X.url} class="text-blue-500 hover:underline">
-							with a direct question on twitter
-						</a>
-						and I&apos;ll respond whenever I can. I will ignore all soliciting.
-					</p>
 				</div>
-			</BlurFade>
-		</div>
-	</section> -->
-</main>
+			</div>
+		</section>
+		<section id="projects">
+			<div class="w-full space-y-12 py-12">
+				<BlurFade delay={BLUR_FADE_DELAY}>
+					<div class="flex flex-col items-center justify-center space-y-4 text-center">
+						<div class="space-y-2">
+							<div class="inline-block rounded-lg bg-foreground px-3 py-1 text-sm text-background">
+								My Projects
+							</div>
+							<h2 class="text-3xl font-bold tracking-tighter sm:text-5xl">
+								Check out my latest work
+							</h2>
+							<p
+								class="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+							>
+								I&apos;ve worked on a variety of projects, from simple websites to complex web
+								applications. Here are a few of my favorites.
+							</p>
+						</div>
+					</div>
+				</BlurFade>
+				<div class="mx-auto grid max-w-[800px] grid-cols-1 gap-3 sm:grid-cols-2">
+					{#each DATA.projects as project, id}
+						<BlurFade delay={BLUR_FADE_DELAY * 1.5 + id * 0.05}>
+							<ProjectCard
+								href={project.href}
+								title={project.title}
+								description={project.description}
+								dates={project.dates}
+								tags={project.technologies}
+								image={project.image}
+								video={project.video}
+								links={project.links}
+							/>
+						</BlurFade>
+					{/each}
+				</div>
+			</div>
+		</section>
+		<!-- <section id="hackathons">
+			<div class="w-full space-y-12 py-12">
+				<BlurFade delay={BLUR_FADE_DELAY}>
+					<div class="flex flex-col items-center justify-center space-y-4 text-center">
+						<div class="space-y-2">
+							<div class="inline-block rounded-lg bg-foreground px-3 py-1 text-sm text-background">
+								Hackathons
+							</div>
+							<h2 class="text-3xl font-bold tracking-tighter sm:text-5xl">I like building things</h2>
+							<p
+								class="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+							>
+								During my time in university, I attended{' '}
+								{DATA.hackathons.length}+ hackathons. People from around the country would come
+								together and build incredible things in 2-3 days. It was eye-opening to see the
+								endless possibilities brought to life by a group of motivated and passionate
+								individuals.
+							</p>
+						</div>
+					</div>
+				</BlurFade>
+				<BlurFade delay={BLUR_FADE_DELAY * 2}>
+					<ul class="mb-4 ml-4 divide-y divide-dashed border-l">
+						{#each DATA.hackathons as project}
+							<BlurFade delay={BLUR_FADE_DELAY}>
+								<HackathonCard {...project} />
+							</BlurFade>
+						{/each}
+					</ul>
+				</BlurFade>
+			</div>
+		</section> -->
+		<!-- <section id="contact">
+			<div class="grid w-full items-center justify-center gap-4 px-4 py-12 text-center md:px-6">
+				<BlurFade delay={BLUR_FADE_DELAY * 2}>
+					<div class="space-y-3">
+						<div class="inline-block rounded-lg bg-foreground px-3 py-1 text-sm text-background">
+							Contact
+						</div>
+						<h2 class="text-3xl font-bold tracking-tight sm:text-5xl">Get in Touch</h2>
+						<p
+							class="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"
+						>
+							Want to chat? Just shoot me a dm
+							<a href={DATA.contact.social.X.url} class="text-blue-500 hover:underline">
+								with a direct question on twitter
+							</a>
+							and I&apos;ll respond whenever I can. I will ignore all soliciting.
+						</p>
+					</div>
+				</BlurFade>
+			</div>
+		</section> -->
+	</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -56,7 +56,20 @@ const config = {
 			},
 			fontFamily: {
 				sans: ['Inter var', ...fontFamily.sans]
-			}
+			},
+			animation: {
+				meteor: "meteor 5s linear infinite",
+			},
+			keyframes: {
+				meteor: {
+				  "0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+				  "70%": { opacity: 1 },
+				  "100%": {
+					transform: "rotate(215deg) translateX(-500px)",
+					opacity: 0,
+				  },
+				},
+			  },
 		}
 	},
 	plugins: [require('@tailwindcss/typography')]
